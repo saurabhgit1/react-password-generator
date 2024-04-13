b2b47a4ca7a008d3770100d05af70f72e452f2d0
saurabhgeminigit1 <saurabh.singh@geminisolutions.com>
2024-04-13 23:00:45 +0530

    pwd generator logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,98 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef("");
+
+  const pwdGenerator = useCallback(() => {
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxjz";
+    if (charAllowed) {
+      string += "!@#$$%&^";
+    }
+    if (numberAllowed) {
+      string += "1234567890";
+    }
+    for (let i = 0; i < length; i++) {
+      const randomNum = Math.floor(Math.random() * string.length);
+      pass += string[randomNum];
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPwdToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0, 3);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(pwdGenerator, [length, numberAllowed, charAllowed, pwdGenerator]);
 
   return (
     <>
-      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      <div
+        className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 
+        py-3 my-8 bg-gray-800 text-orange-500"
+      >
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            value={password}
+            ref={passwordRef}
+          />
+          <button
+            className="outline-none bg-blue-700 
+          text-white px-3 py-0.5 shrink-0"
+            onClick={copyPwdToClipboard}
+          >
+            copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              className="cursor-pointer"
+              onChange={(event) => setLength(event.target.value)}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="charInput"
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="charInput">Characters</label>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
